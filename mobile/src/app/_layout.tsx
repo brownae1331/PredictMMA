88ee7f3f2be23afa205b32bba7d358b266912cc1
brownae1331 <brownae1331@gmail.com>
2025-06-14 18:05:45 +0100

    Start of make prediction page, fighter and method of victory containers

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="event-details" options={{ headerShown: false }} />
+      <Stack.Screen name="make-prediction" options={{ headerShown: false }} />
     </Stack>
   );
 }
